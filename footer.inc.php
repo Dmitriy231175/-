a9fff93c15184f3472b7ab8dae0d5d4dd7aba1e5
Dmitriy231175 <121481719+Dmitriy231175@users.noreply.github.com>
2022-12-29 12:12:06 +0300

    Add files via upload

---
+++ footer.inc.php
@@ -0,0 +1,3 @@
+<div class="footer">
+            <h2>Мы изучили основы PHP!</h2>
+        </div>
---
+++ main.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Practice </title>
+    <link rel="stylesheet" href="style.css" />
+</head>
+<body>
+    
+    <div class="flex-container">
+
+        <div class="header">     
+               <?php include 'logo.inc.php' ?>         
+               <?php include 'menu.inc.php' ?>     
+        </div>       
+     
+        <div class="about_me">
+         
+          <h1>  <?php  echo $p  ?> </h1>
+
+            <div class="data">
+                <div class="myImg">
+                    <?php
+                    echo '<img src="/img/php.jpg">'; 
+                    ?>                    
+                </div>
+
+                <div class="fullname">
+                    <p> Меня зовут 
+                    <?php echo $name, ' ', $surname . '<br>'; 
+                          echo 'город', ' ', $city; ?>                                      
+                    </p> 
+           
+                    <p> Мне
+                    <?php  echo $age;   ?>          
+                    лет </p>
+                    <p> Мы научились создавать переменные </p>
+                    <p> Изучили простые операции с ними </p>
+                </div>
+            </div>
+
+            <div class="knowledge">
+                                   
+                    <?php  include 'knowledge.inc.php'; ?>
+                    <?php   
+                    echo $a, ' ', $b, ' ', $c; 
+                    ?> <br>
+                                       
+                    <?php
+                        $a = 10;
+                        $b = 20;
+                        $c = $a + $b;
+                        echo $c;
+                    ?>   <br>                
+                     <?php
+                        echo $d;
+                    ?> 
+
+            </div>
+
+            <div class="article">
+                <p class="text">
+                    Я работаю на транспорте, учусь на белого хакера 
+                </p>
+            </div>
+        </div>
+
+            <?php include 'footer.inc.php' ?>
+
+    </div>
+
+
+</body>
+</html>
